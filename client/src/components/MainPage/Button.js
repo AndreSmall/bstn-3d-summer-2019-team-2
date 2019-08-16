7da0b256ee7ed71b0b0d3751e4528f197ab50bd9
yuri gwon <ilchanggwon@gmail.com>
2019-08-16 13:44:39 -0400

    button completed

---
+++ client/src/components/MainPage/Button.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Button.scss";
+
+class Button extends React.Component {
+  state = { colorChange: false };
+  toggle = () => {
+    if (this.state.colorChange === false) {
+      this.setState({ colorChange: true });
+    } else if (this.state.colorChange === true) {
+      this.setState({ colorChange: false });
+    }
+  };
+  render() {
+    let classVariable;
+    let buttonBlue = "button-color";
+    let buttonRed = "button-red";
+    if (this.state.colorChange === true) {
+      classVariable = buttonRed;
+    } else if (this.state.colorChange === false) {
+      classVariable = buttonBlue;
+    }
+    return (
+      <>
+        <button className={classVariable} onClick={this.toggle} type="button">
+          
+        </button>
+      </>
+    );
+  }
+}
+
+export default Button;
