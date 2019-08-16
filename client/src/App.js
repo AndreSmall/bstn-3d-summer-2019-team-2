10a3031337554e8448bd17ac4f3200afcbf91a40
yuri gwon <ilchanggwon@gmail.com>
2019-08-16 13:56:57 -0400

    Button component completed/ MainPage component is Demo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import MainPage from "./components/MainPage/MainPage";
 function App() {
   return (
    <>
-    
+    <MainPage />
     </>
   );
 }
--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,31 +1,17 @@
 import React from "react";
 import './MainPage.scss';
-
+import Button from "./Button"
 class MainPage extends React.Component {
-    state = { colorChange: false }
-    toggle = () => {
-        if (this.state.colorChange === false) {
-            this.setState({ colorChange : true })
-        } else if (this.state.colorChange === true) {
-            this.setState({ colorChange : false})
-        }
-    }
+  
     render () {
-        let classVariable;
-        let buttonBlue = "button-color";
-        let buttonRed = "button-red";
-        if (this.state.colorChange === true) {
-            classVariable = buttonRed
-        } else if (this.state.colorChange === false) {
-            classVariable = buttonBlue
-        }
+        
         return (
             <>
             <div className="mainpage-wrapper">
             <input type="text" name="mainPageInput" placeholder="Search"/>
             <button className="mainpage-wrapper__button" onClick={this.toggle} type="button" name="mainPageButton">Enter</button>
-            <button className={classVariable} onClick={this.toggle} type="button">toggle</button>
             </div>
+            <Button />
             </>
         )
     }
